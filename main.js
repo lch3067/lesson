696c3b736ec20b072b9d42bf0893c7e20a7a0a80
이충현 <lch3067@naver.com>
2025-07-06 15:33:46 +0900

    감사합니다. 3번 출력하게 하는 이벤트 기능 만들기

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 function jstest() {
-    alert("감사합니다.");
+
+    //  시간이 없어서 미리 짜두었습니다.
+    let arry = new Array();
+
+    arry.push(1);
+    arry.push(1);
+    arry.push(1);
+
+    for(let i = 0; i < arry.length; i++) {
+        console.log("감사합니다." + i);
+    }
+    
 }
